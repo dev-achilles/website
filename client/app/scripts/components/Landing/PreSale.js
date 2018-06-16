fd42f4bd23b291ef07e0c7b3a8b51c03fb1f3980
Duong Le <navi.ocean@outlook.com>
2018-06-16 16:29:58 +0700

    update sub header of presales

--- client/app/scripts/components/Landing/PreSale.js
+++ client/app/scripts/components/Landing/PreSale.js
@@ -2,10 +2,11 @@
  * Project: react-boilerplate
  * Author: Duong Le (navi.ocean@outlook.com)
  * File Created: Saturday, 31st March 2018 12:51:53 am
- * Last Modified: Sunday, 10th June 2018 9:13:14 am
+ * Last Modified: Saturday, 16th June 2018 4:27:57 pm
  */
 import React, { Component } from 'react';
 import moment from 'moment';
+import ReactHtmlParser from 'react-html-parser';
 import Button from '../Button';
 import CountDown from './CountDown';
 
@@ -20,7 +21,7 @@ class PreSale extends Component {
             <div className="row justify-content-center align-items-center">
               <div className="col-md-6 mx-auto">
                 <h2 className="section-heading">{d.heading}</h2>
-                <h3>{d.sub}</h3>
+                <div className="section-sub">{ReactHtmlParser(d.sub)}</div>
                 <CountDown date={moment(d.endTime).format('MM/DD/YYYY')} />
                 {d.button.show ? (
                   <a href={d.button.link}>
